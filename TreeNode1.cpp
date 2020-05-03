e11aa2fd6c71822468c08355a6f08b252168c66f
Yuki Chen <yingchen@chapman.edu>
2020-05-02 23:47:20 -0700

    add

---
+++ TreeNode1.cpp
@@ -0,0 +1,17 @@
+//default constructor
+TreeNode1::TreeNode1(){
+  key=0;
+  left=NULL;
+  right=NULL;
+}
+//overload constructor
+TreeNode1::TreeNode1(int k){
+  key=k;
+  left=NULL;
+  right=NULL;
+}
+//destructor
+TreeNode1::~TreeNode1()
+{
+  //figure it out myself
+}
