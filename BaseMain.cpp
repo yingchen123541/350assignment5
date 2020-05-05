ec5bc2554113f60f241e0418e322f0097345e049
Yuki Chen <yingchen@chapman.edu>
2020-05-04 23:53:02 -0700

    add

--- BaseMain.cpp
+++ BaseMain.cpp
@@ -1,2 +1,50 @@
 //open and read from two files (student and faculty) and add information into trees
-//if those two files don't exist initialize two empty trees 
+//if those two files don't exist initialize two empty trees
+// tree name: masterFaculty, masterStudent
+
+
+#include "StudentRecord.cpp"
+#include "FacultyRecord.cpp"
+
+
+
+using namespace std;
+
+int main(){
+  string c;
+  string d;
+  //this should print out the stuffs I added into trees in studentrecord and facultyrecord class
+  //but my tree classes are not working 
+  c->addStudentTree();
+  d->addFacultyTree();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  return 0;
+
+}//end main
