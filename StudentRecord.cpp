3e897332cbc1e7637b67e5732ca73c746cfe740e
Yuki Chen <yingchen@chapman.edu>
2020-05-05 19:49:57 -0700

    only read ID from each line

--- StudentRecord.cpp
+++ StudentRecord.cpp
@@ -1,32 +1,39 @@
 #include<fstream>
 #include "BSTree1.cpp"
 
-string addStudentTree(){
+//string addStudentTree(){
 
 //open and read from student file
+
   ifstream StudentFile;
   StudentFile.open("studentTable.txt");
   string StudentLine;
+  string StudentID;
 
 
   if(!StudentFile)
   {
     cerr << "student file doesn't exist, empty tree" << endl;
     //initialize empty student tree
-    BSTree1 *masterStudent = new BSTree1(10);
+  //  BSTree1 *masterStudent = new BSTree1(10);
   }//end if
   else if (StudentFile)
   {
     //studentTable successfully opened, add content into masterStudent tree
-    BSTree1 *masterStudent = new BSTree1(10);
+  //  BSTree1 *masterStudent = new BSTree1(10);
     while (getline(StudentFile, StudentLine))
     {
-      //for now only print out information line by line
-      //maybe we can search through each line and do .contains(studentID) when need to print out information base on ID
-      //or should we seperate each line by comma? but how to store in trees? how many tree nodes to use for one student's information?
-    cout << StudentLine << endl;
-    masterStudent->insert(StudentLine);
+      for(int z=0; z < 1; z++)
+      {
+        //only read the first character in each line
+           StudentID = StudentLine.substr(z,1);
+          cout << StudentLine[z] << endl;
+
+  //  masterStudent->insert(StudentLine);
     }// end while
   }//end else if
-  return masterStudent;
+  StudentFile.close();
+
+  //return masterStudent;
+//}
 }
